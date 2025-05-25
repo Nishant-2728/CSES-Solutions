edf7cd8236a8050a58ca320f99ad5edb2fb6f2c5
Nishant-2728 <biswalnishant2003@gmail.com>
2025-05-25 06:04:35 +0000

    config done

--- DynamicProgramming/MinimizingCoins.cpp
+++ DynamicProgramming/MinimizingCoins.cpp
@@ -17,11 +17,16 @@ long long int Totalways(long long int x,long long int c[],long long int n,vector
 int main(){
     long long int n,x;
     cin>>n>>x;
-    long long int c[n],j;
+    long long int c[n],j,k;
     for(j=0;j<n;j++){
         cin>>c[j];
     }
-    vector<long long int>dp(x+1,-1);
-    long long int ans=Totalways(x,c,n,dp);
-    cout<<(ans<1e17?ans:-1)<<endl;
+    vector<long long int>dp(x+1,1e17);
+    dp[0]=0;
+    for(j=1;j<=x;j++){
+        for(k=0;k<n;k++){
+            dp[j]=min(dp[j],(j-c[k]>=0?dp[j-c[k]]+1:(long long int)1e17));
+        }
+    }
+    cout<<(dp[x]>=1e17?-1:dp[x])<<endl;
 }
