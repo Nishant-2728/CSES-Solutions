0e50873361d843845b79231985e5895830ae1903
Nishant-2728 <biswalnishant2003@gmail.com>
2025-08-04 19:04:07 +0000

    config done

---
+++ GraphAlgorithms/MailDelivery.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+void dfs(int node,set<int>adj[],vector<int>&ans){
+    while(adj[node].size()>0){
+        auto it=adj[node].begin();
+        int adjNode=*it;
+        adj[node].erase(adjNode);
+        adj[adjNode].erase(node);
+        dfs(adjNode,adj,ans);
+    }
+    ans.push_back(node);
+}
+int main(){
+    int n,m;
+    cin>>n>>m;
+    set<int>adj[n+1];
+    for(int j=0;j<m;j++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].insert(v);
+        adj[v].insert(u);
+    }
+    int count=0;
+    for(int j=1;j<=n;j++){
+        if(adj[j].size()%2){
+            count++;
+        }
+    }
+    if(count>0){
+        cout<<"IMPOSSIBLE"<<endl;
+    }
+    else{
+        vector<int>ans;
+        dfs(1,adj,ans);
+        for(auto it:ans){
+            cout<<it<<" ";
+        }
+        cout<<endl;
+    }
+}
