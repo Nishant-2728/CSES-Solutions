500054d378a8ac4c51f4b5c290b8ee8cd438e0b3
Nishant Kumar Biswal <biswalnishant2003@gmail.com>
2025-10-02 20:33:27 +0530

    Create RangeXorQueries.cpp

---
+++ RangeQueries/RangeXorQueries.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long int n,q;
+    cin>>n>>q;
+    long long int a[n],j;
+    for(j=0;j<n;j++){
+        cin>>a[j];
+        if(j)
+            a[j]^=a[j-1];
+    }
+    for(j=0;j<q;j++){
+        int x,b;
+        cin>>x>>b;
+        x--;
+        b--;
+        cout<<(a[b]^(x-1>=0?a[x-1]:0))<<"\n";
+    }
+}
